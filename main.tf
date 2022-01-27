129442a7cb4d96800572127e3cd0e8102ba7793f
steph y <yee.stephanie.m@gmail.com>
2022-01-27 23:58:27 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -27,6 +27,7 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.myresourcegroup.name
   tags = {
     Department = "devops"
+    Billable = "true"
   }
 }
 
