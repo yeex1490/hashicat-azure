0c7e35741c07cf0acc3db0c6706485f6075c228c
steph y <yee.stephanie.m@gmail.com>
2022-01-28 00:25:30 +0000

    Added network module

--- main.tf
+++ main.tf
@@ -25,10 +25,7 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.myresourcegroup.location
   address_space       = [var.address_space]
   resource_group_name = azurerm_resource_group.myresourcegroup.name
-  tags = {
-    Billable = "true",
-    Department = "devops"
-  }
+
 }
 
 resource "azurerm_subnet" "subnet" {
