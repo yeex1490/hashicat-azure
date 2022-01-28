5fcd7b4eaf758d20baf671d5d3244e45dd8d9ab5
steph y <yee.stephanie.m@gmail.com>
2022-01-28 00:07:37 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -25,10 +25,10 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.myresourcegroup.location
   address_space       = [var.address_space]
   resource_group_name = azurerm_resource_group.myresourcegroup.name
-  mandatory_tags = [
+  mandatory_tags = {
     Billable = "true",
     Department = "devops"
-  ]
+  }
 }
 
 resource "azurerm_subnet" "subnet" {
